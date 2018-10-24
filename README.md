2fdfab4d45f609918418845593bca04af19a2a8b
Fatih Sarhan <f9n@protonmail.com>
2018-10-24 17:23:18 +0300

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# gamegos-supervisor
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: gamegos-supervisor
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
