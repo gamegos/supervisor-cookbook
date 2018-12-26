56bc6efcdf95e7140bcbe3add95e136c1f30e743
f9n <f9n@protonmail.com>
2018-12-26 18:56:25 +0300

    Add supervisord_executable_path property to supervisor_service resource

--- resources/service.rb
+++ resources/service.rb
@@ -1,8 +1,9 @@
 # To learn more about Custom Resources, see https://docs.chef.io/custom_resources.html
 resource_name :supervisor_service
+property :supervisord_executable_path, String, default: '/usr/local/bin/supervisord'
 
 action :create do
   poise_service 'supervisor' do
-    command lazy { "/usr/local/bin/supervisord -n -c #{node.run_state['supervisor']['config_file']}" }
+    command lazy { "#{new_resource.supervisord_executable_path} -n -c #{node.run_state['supervisor']['config_file']}" }
   end
 end
