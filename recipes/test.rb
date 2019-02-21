b6616daf461396f429137b0cdd32cba6378f62cb
Andrew Bobulsky <andrew.bobulsky@buyerquest.com>
2019-02-21 16:48:09 -0500

    Add tests and Dokken support
    We test as many aspects as possible of the installed supervisord service 
    and its support binaries. We've also switched to Dokken when possible 
    because it tests things a little faster than Vagrant.
    
    Vagrant tests include a partial Vagrantfile designed to speed up testing 
    in Virtualbox.
    
    Added a test recipe to use for testing.

---
+++ recipes/test.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook:: gamegos-supervisor
+# Recipe:: test
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+supervisor_install '3.3'
+
+supervisor_config 'supervisor'
+
+supervisor_process 'cat' do
+  type 'program'
+  command '/bin/cat'
+end
+
+supervisor_process 'vi' do
+  type 'program'
+  command '/usr/bin/vi'
+end
+
+supervisor_group 'cat' do
+  programs ['cat']
+end
+
+supervisor_group 'vi' do
+  programs ['vi']
+end
+
+supervisor_service 'supervisor'
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,18 +1,76 @@
-# # encoding: utf-8
-
-# Inspec test for recipe gamegos-supervisor::default
+# Inspec test for recipe gamegos-supervisor::test
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+if os.family == 'debian'
+  supervisor_bin_directory = '/usr/local/bin'
+  python_package = 'python2.7'
+elsif os.family == 'redhat'
+  supervisor_bin_directory = '/usr/bin'
+  python_package = 'python'
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe package(python_package) do
+  it { should be_installed }
+end
+
+describe pip('supervisor') do
+  it { should be_installed }
+end
+
+describe service('supervisor') do
+  it { should be_running }
+end
+
+describe file("#{supervisor_bin_directory}/supervisord") do
+  it { should exist }
+end
+
+describe file("#{supervisor_bin_directory}/supervisorctl") do
+  it { should exist }
+end
+
+describe file('/etc/supervisor/supervisord.conf') do
+  it { should exist }
+end
+
+describe command('supervisord -h') do
+  its(:stdout) { should match 'supervisord -- run a set of applications as daemons.' }
+  its('exit_status') { should eq 0 }
+end
+
+describe command('supervisorctl -h') do
+  its(:stdout) { should match 'supervisorctl -- control applications run by supervisord from the cmd line' }
+  its('exit_status') { should eq 0 }
+end
+
+%w[cat vi].each do |process|
+  describe file("/etc/supervisor/program_#{process}.conf") do
+    it { should exist }
+  end
+
+  describe file("/etc/supervisor/group_#{process}.conf") do
+    it { should exist }
+  end
+
+  describe command("supervisorctl status #{process}") do
+    its(:stdout) { should match "#{process} *STOPPED\s" }
+    its('exit_status') { should eq 0 }
+  end
+
+  describe command("supervisorctl start #{process}") do
+    its(:stdout) { should match "#{process}: started" }
+    its('exit_status') { should eq 0 }
+  end
+
+  describe command("supervisorctl status #{process}") do
+    its(:stdout) { should match "#{process} *RUNNING\s" }
+    its('exit_status') { should eq 0 }
+  end
+
+  describe command("supervisorctl stop #{process}") do
+    its(:stdout) { should match "#{process}: stopped" }
+    its('exit_status') { should eq 0 }
+  end
 end
